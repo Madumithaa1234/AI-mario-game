615a1bf2e2ad018e0284ad647c5960865674b85a
Madumithaa1234 <89779017+Madumithaa1234@users.noreply.github.com>
2023-01-13 18:33:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+img = "";
+marioX = 325;
+marioY = 325;
+noseX = 0;
+noseY = 0;
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+	img = loadImage("mario01.png")
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(600, 300);
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function gotposes(results){
+    if(results.length > 0){
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX = " + noseX, "noseY = " + noseY);
+
+	}
+}
+
+function modelLoaded(){
+	console.log(modelLoaded);
+}
+
+
+function draw() {
+	game()
+	background("#470a80");
+	Image(img,marioX, marioY, 40, 70);
+}
+
+
+
+
+
+
